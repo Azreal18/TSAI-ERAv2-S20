c87fbf095356a60896ad25fd3207b7e6a709bfc7
Viraj Bhanushali <104625176+Azreal18@users.noreply.github.com>
2024-08-20 21:23:09 +0530

    chore: Add Gradio interface for token generation in Hindi dataset

---
+++ app.py
@@ -0,0 +1,42 @@
+import gradio as gr
+import os
+from tokenizer_bpe import BasicTokenizer
+
+print("Loading the model...")
+model_path = os.path.join(os.getcwd(), "tokenizer_model")
+model_path = os.path.join(model_path, "hindi_sentiments.model")
+
+basic_tokenizer = BasicTokenizer()
+basic_tokenizer.load(model_path)
+
+
+def test_tokenizer(text):
+    ids = basic_tokenizer.encode(text)
+    decoded = basic_tokenizer.decode(ids)
+    mapping = [(str(i), basic_tokenizer.decode([i])) for i in ids]
+
+    return ids, decoded, mapping
+
+
+with gr.Blocks() as demo:
+    gr.HTML("<h2 align = 'center'> Token Generation for Hindi Dataset </h2>")
+
+    with gr.Row():
+        with gr.Column():
+            inputs = [
+                gr.TextArea(
+                    label="Enter text to generate tokens in Hindi", lines=10
+                )
+            ]
+            generate_btn = gr.Button(value="Generate Text")
+        with gr.Column():
+            enc = gr.Textbox(label="Encoded Tokens")
+            txt = gr.Textbox(label="Decoded Text from tokens")
+            map = gr.Textbox(label="Mapping of the tokens and respective texts")
+            outputs = [enc, txt, map]
+
+    generate_btn.click(fn=test_tokenizer, inputs=inputs, outputs=outputs)
+
+
+if __name__ == "__main__":
+    demo.launch(share=True)
